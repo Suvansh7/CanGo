93e8f16c8c81809384cd64633923692d21b5ec4a
ansh verma <anshv309@gmail.com>
2022-12-30 22:06:31 +0530

    hih

--- app/src/main/java/com/example/cango/MainActivity.java
+++ app/src/main/java/com/example/cango/MainActivity.java
@@ -1,233 +1,24 @@
 package com.example.cango;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.InputType;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-    Button stu;
-    Button tea;
-    Button gua;
-    EditText number;
-    DatabaseReference reference;
-    private String no,tkey,gkey;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-        stu = findViewById(R.id.button);
-        stu.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                popupstu();
+            public void run() {
+                startActivity(new Intent(MainActivity.this, splash.class));
+                finish();
             }
-        });
-        tea = findViewById(R.id.button2);
-        tea.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                popuptea();
-            }
-        });
-        gua = findViewById(R.id.button3);
-        gua.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                popupgua();
-            }
-        });
+        }, 3000);
     }
-
-
-    private void popupstu(){
-        stu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                    AlertDialog.Builder mydialog = new AlertDialog.Builder(MainActivity.this);
-                    mydialog.setTitle("Enter your Phone no.");
-
-                    final EditText Phoneinput = new EditText(MainActivity.this);
-                    Phoneinput.setInputType(InputType.TYPE_CLASS_PHONE);
-                    mydialog.setView(Phoneinput);
-
-                    mydialog.setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            no=Phoneinput.getText().toString();     //the phone no entered by user
-//                            loadingDialog.startLoadingDialog();
-                            if(no.length()==10){
-                                reference= FirebaseDatabase.getInstance().getReference("Students");
-
-                                reference.child(no).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<DataSnapshot> task) {
-                                        if(task.isSuccessful()){
-
-                                            if(task.getResult().exists()){
-
-                                                DataSnapshot dataSnapshot= task.getResult();
-                                                String ph= String.valueOf(dataSnapshot.child("phone_no").getValue());
-                                                if(ph.equals(no)){
-//                                                    loadingDialog.dismissDialog();
-                                                    Toast.makeText(MainActivity.this, "Phone No Exists!", Toast.LENGTH_SHORT).show();
-                                                    Intent extraIntent = new Intent(MainActivity.this, signin.class);
-                                                    extraIntent.putExtra("keyNo", no);
-                                                    startActivity(extraIntent);
-                                                }
-                                            }
-                                            else{
-//                                                loadingDialog.dismissDialog();
-                                                Toast.makeText(MainActivity.this, "Data does not exist", Toast.LENGTH_SHORT).show();
-                                                Intent extraIntent = new Intent(MainActivity.this, signup.class);
-                                                extraIntent.putExtra("keyNo", no);
-                                                startActivity(extraIntent);
-                                            }
-                                        }
-                                    }
-                                });
-
-                            }
-                            else{
-                                Toast.makeText(MainActivity.this, "Phone Number should be of 10 digits!", Toast.LENGTH_SHORT).show();
-//                                loadingDialog.dismissDialog();
-                            }
-
-
-                        }
-                    });
-
-                    mydialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            dialogInterface.cancel();
-                        }
-                    });
-                    mydialog.show();
-
-
-
-            }
-
-        });
-
-    }
-
-
-    private void popuptea(){
-        tea.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder mydialog = new AlertDialog.Builder(MainActivity.this);
-                mydialog.setTitle("Enter Key");
-
-                final EditText Phoneinput = new EditText(MainActivity.this);
-                Phoneinput.setInputType(InputType.TYPE_CLASS_PHONE);
-                mydialog.setView(Phoneinput);
-
-                mydialog.setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        tkey=Phoneinput.getText().toString();     //the phone no entered by user
-//                            loadingDialog.startLoadingDialog();
-                        if(tkey.equals("1111")){
-
-
-                            Toast.makeText(MainActivity.this, "Verified", Toast.LENGTH_SHORT).show();
-                            Intent extraIntent = new Intent(MainActivity.this, Requests.class);
-                            extraIntent.putExtra("keyNo", no);
-                            startActivity(extraIntent);
-                        }
-                        else{
-                            Toast.makeText(MainActivity.this, "Not Matched!", Toast.LENGTH_SHORT).show();
-//                                loadingDialog.dismissDialog();
-                        }
-
-
-                    }
-                });
-
-                mydialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.cancel();
-                    }
-                });
-                mydialog.show();
-
-
-
-            }
-
-        });
-
-    }
-
-
-    private void popupgua(){
-        gua.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder mydialog = new AlertDialog.Builder(MainActivity.this);
-                mydialog.setTitle("Enter Key");
-
-                final EditText Phoneinput = new EditText(MainActivity.this);
-                Phoneinput.setInputType(InputType.TYPE_CLASS_PHONE);
-                mydialog.setView(Phoneinput);
-
-                mydialog.setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        gkey=Phoneinput.getText().toString();     //the phone no entered by user
-//                            loadingDialog.startLoadingDialog();
-                        if(gkey.equals("0000")){
-
-
-                            Toast.makeText(MainActivity.this, "Verified", Toast.LENGTH_SHORT).show();
-//                            Intent extraIntent = new Intent(MainActivity.this, Requests.class);
-//                            extraIntent.putExtra("keyNo", no);
-//                            startActivity(extraIntent);
-                        }
-                        else{
-                            Toast.makeText(MainActivity.this, "Not Matched!", Toast.LENGTH_SHORT).show();
-//                                loadingDialog.dismissDialog();
-                        }
-
-
-                    }
-                });
-
-                mydialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.cancel();
-                    }
-                });
-                mydialog.show();
-
-
-
-            }
-
-        });
-
-    }
-
-
 }
