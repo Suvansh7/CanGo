8e8601ef10f63f4ff04748dd9e4ace845a581d83
Utkar <utkarsh>
2022-12-30 17:50:13 +0530

    utkarsh

--- app/src/main/java/com/example/cango/studentReq.java
+++ app/src/main/java/com/example/cango/studentReq.java
@@ -3,12 +3,39 @@ package com.example.cango;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class studentReq extends AppCompatActivity {
+
+    EditText Reason;
+    Button Submit;
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
+    DatabaseReference root = db.getReference().child("Permission");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_req);
+
+
+
+        Reason = findViewById(R.id.editTextTextMultiLine);
+        Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String reason = Reason.getText().toString();
+
+                HashMap<String , String> userMap = new HashMap<>();
+                userMap.put("name" , reason);
+            }
+        });
     }
 }
