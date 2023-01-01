d5bd526291420f2f5f9c18e9b89e0ff94b10a842
ansh verma <anshv309@gmail.com>
2023-01-01 23:12:18 +0530

    hih

--- app/src/main/java/com/example/cango/MainActivity.java
+++ app/src/main/java/com/example/cango/MainActivity.java
@@ -8,19 +8,23 @@ import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class MainActivity extends AppCompatActivity {
+    LottieAnimationView lottie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+        lottie = findViewById(R.id.animationView);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(MainActivity.this, splash.class));
                 finish();
             }
-        }, 3000);
+        }, 5000);
     }
 }
